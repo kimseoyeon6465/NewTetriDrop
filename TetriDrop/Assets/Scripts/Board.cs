2f5c4894ef10fb38b0425d9fb406b25a06191507
kimseoyeon6465 <48300652+kimseoyeon6465@users.noreply.github.com>
2025-07-30 13:53:33 +0900

    Add GameOver and Ghost Piece

--- TetriDrop/Assets/Scripts/Board.cs
+++ TetriDrop/Assets/Scripts/Board.cs
@@ -1,5 +1,6 @@
 
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
@@ -37,9 +38,20 @@ public class Board : MonoBehaviour
         int random = Random.Range(0, tetrominoes.Length);
         TetrominoData data = this.tetrominoes[random];
         this.activePiece.Initialize(this, this.spawnPosition, data);
-        Set(this.activePiece);
+        if(IsValidPosition(this.activePiece, this.spawnPosition))
+        {
+            Set(this.activePiece);
+        }
+        else
+        {
+            GameOver();
+        }
     }
-
+    private void GameOver()
+    {
+        Debug.Log("GameOver");
+        this.tilemap.ClearAllTiles();
+    }
     public void Set(Piece piece)
     {
         for (int i = 0; i < piece.cells.Length; i++)
@@ -76,5 +88,59 @@ public class Board : MonoBehaviour
 
         }
         return true;
+    }
+
+    public void ClearLines()
+    {
+        RectInt bounds = this.Bounds;
+        int row = bounds.yMin;
+
+        while(row<bounds.yMax)
+        {
+            if(IsLineFull(row))
+            { LineClear(row); }
+            else
+            {
+                row++;
+            }
+        }
+    }
+
+    private bool IsLineFull(int row)
+    {
+        RectInt bounds = this.Bounds;
+        for(int col= bounds.xMin; col<bounds.xMax;col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            if (!this.tilemap.HasTile(position))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void LineClear(int row)
+    {
+        RectInt bounds = this.Bounds;
+        for(int col= bounds.xMin; col < bounds.xMax; col++)
+        {
+            Vector3Int position = new Vector3Int(col, row, 0);
+            this.tilemap.SetTile(position,null);
+        }
+
+        while(row < bounds.yMax)
+        {
+            for(int col= bounds.xMin; col < bounds.xMax; col++)
+            {
+                Vector3Int position = new Vector3Int(col, row+1, 0);
+                TileBase above = this.tilemap.GetTile(position);
+
+                position = new Vector3Int(col, row, 0);
+                this.tilemap.SetTile(position, above);
+            }
+
+            row++;
+        }
     }
 }
--- TetriDrop/Assets/Scripts/Piece.cs
+++ TetriDrop/Assets/Scripts/Piece.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,20 @@ public class Piece : MonoBehaviour
     public Vector3Int[] cells { get; private set; }
     public Vector3Int position { get; private set; }
     public int rotationIndex { get; private set; }
+
+    public float stepDelay = 1f;
+    public float lockDelay = 0.5f;
+
+    private float stepTime;
+    private float lockTime;
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
         this.rotationIndex = 0;
+        this.stepTime=Time.time + this.stepDelay;
+        this.lockTime = 0f;
 
         if (this.cells == null)
         {
@@ -31,6 +40,9 @@ public class Piece : MonoBehaviour
     private void Update()
     {
         this.board.Clear(this);
+        this.lockTime += Time.deltaTime;   
+
+        
         if (Input.GetKeyDown(KeyCode.Q))
         {
             Rotate(-1);
@@ -54,7 +66,31 @@ public class Piece : MonoBehaviour
         {
             HardDrop();
         }
+
+        if(Time.time >=this.stepTime)
+        {
+            Step();
+        }
         this.board.Set(this);
+    }
+
+    private void Step()
+    {
+        this.stepTime = Time.time + this.stepDelay;
+
+        Move(Vector2Int.down);
+
+        if (this.lockTime >= this.lockDelay)
+        {
+            Lock();
+        }
+    }
+
+    private void Lock()
+    {
+        this.board.Set(this);
+        this.board.ClearLines();    
+        this.board.SpawnPiece();
     }
     private void Rotate(int direction)
     {
@@ -137,6 +173,7 @@ public class Piece : MonoBehaviour
             continue;
         }
 
+        Lock();
     }
     private bool Move(Vector2Int translation)
     {
@@ -148,6 +185,7 @@ public class Piece : MonoBehaviour
         if (valid)
         {
             this.position = newPosition;
+            this.lockTime = 0f;
         }
         return valid;
     }
